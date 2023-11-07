cdeefec0395ab51e0db6a4ff7d74a40a3b160930
= <kimchankwon@gmail.com>
2023-11-07 19:14:46 +1100

    a

--- frontend/src/app/(pages)/group/[groupId]/page.tsx
+++ frontend/src/app/(pages)/group/[groupId]/page.tsx
@@ -5,8 +5,8 @@ export default function Group({ params }: { params: { groupId: string } }) {
       Group id: {params.groupId}
       <h1>Daniel L - SOW-402: TODO Metrics</h1>
       <h1>
-        Ian - SOW-419: TODO Add this group to list if it is the user's first
-        time going onto this group
+        Ian - SOW-419: TODO Add this group to list if it is the users first time
+        going onto this group
       </h1>
       <h1>Dominic - SOW-416: TODO Group settings</h1>
     </main>
