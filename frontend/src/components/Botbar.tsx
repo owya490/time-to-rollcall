d340a68d6f52c748bbd78da8b4f04f2d4104b7df
= <kimchankwon@gmail.com>
2023-12-04 13:55:01 +1100

    let bottom extend out

--- frontend/src/components/Botbar.tsx
+++ frontend/src/components/Botbar.tsx
@@ -7,7 +7,7 @@ import { Path } from "helper/Path";
 export default function Botbar() {
   const user = useContext(UserContext);
   return (
-    <nav className="bg-neutral-100 fixed w-full z-20 bottom-0 start-0">
+    <nav className="bg-neutral-100 fixed w-full pb-96 z-20 -bottom-96 start-0">
       <div className="flex flex-wrap items-center justify-between mx-auto px-8 py-6">
         <Link
           type="button"
