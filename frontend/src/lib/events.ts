0e7040747e64f72c9528aea918a1b2615f20c956
Daniel Lee <daniel.lee@sowaustralia.com>
2024-02-12 23:07:25 +1100

    SOW-410: Added method to get events by tag

--- frontend/src/lib/events.ts
+++ frontend/src/lib/events.ts
@@ -1,4 +1,4 @@
-import { collection, getDocs, orderBy, query } from "firebase/firestore";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 import { convertCollectionToJavascript, firestore } from "@/lib/firebase";
 import { EventModel } from "@/models/Event";
 
@@ -6,6 +6,18 @@ export async function getEvents(groupId: string) {
   const events = await getDocs(
     query(
       collection(firestore, "groups", groupId, "events"),
+      orderBy("dateEnd", "desc"),
+      orderBy("dateStart", "desc")
+    )
+  );
+  return convertCollectionToJavascript(events.docs) as EventModel[];
+}
+
+export async function getEventsbyTag(groupId: string, tagId: string) {
+  const events = await getDocs(
+    query(
+      collection(firestore, "groups", groupId, "events"),
+      where("tags", "in", [tagId]),
       orderBy("dateEnd", "desc"),
       orderBy("dateStart", "desc")
     )
