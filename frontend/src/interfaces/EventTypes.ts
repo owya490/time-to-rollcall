df306954dcb0e019f5b585e39c41541f589c072b
Owen <oyang@atlassian.com>
2024-02-18 01:25:31 +1100

    fleshed out backend service layer

--- frontend/src/interfaces/EventTypes.ts
+++ frontend/src/interfaces/EventTypes.ts
@@ -1,3 +1,16 @@
+import { Timestamp } from "firebase/firestore";
+import { AttendeeId } from "./AttendeeTypes";
+
 export type EventId = string;
 
 export type GroupId = string;
+
+export interface Event {
+    groupId: GroupId;
+    eventId: EventId;
+    name: string;
+    dateStart: Timestamp;
+    dateEnd: Timestamp;
+    tags: string[];
+    attendees: AttendeeId[];
+}
--- frontend/src/services/eventService.ts
+++ frontend/src/services/eventService.ts
@@ -1,60 +1,81 @@
 import { firestore } from "@/lib/firebase";
 import {
-  addDoc,
-  collection,
-  deleteField,
-  doc,
-  getDocs,
-  updateDoc,
+    addDoc,
+    arrayRemove,
+    arrayUnion,
+    collection,
+    doc,
+    getDoc,
+    updateDoc,
 } from "firebase/firestore";
 import { Attendee, AttendeeId } from "interfaces/AttendeeTypes";
-import { EventId, GroupId } from "interfaces/EventTypes";
+import { Event, EventId, GroupId } from "interfaces/EventTypes";
 
-export async function getAttendanceList(eventId: EventId): Promise<Attendee[]> {
-  const snapshot = await getDocs(collection(firestore, "Events", eventId));
-  const attendanceList = [];
-  snapshot.forEach((doc) => {
-    attendanceList.push(doc.data() as Attendee);
-  });
-  return attendanceList;
+export async function getEventById(groupId: GroupId, eventId: EventId) {
+    const snapshot = await getDoc(
+        doc(firestore, "groups", groupId, "events", eventId)
+    );
+    const fetchedEvent = snapshot.data() as Event;
+    fetchedEvent.eventId = eventId;
+    fetchedEvent.groupId = groupId;
+    return fetchedEvent;
+}
+
+export async function getAttendeeById(attendeeId: AttendeeId) {
+    const snapshot = await getDoc(doc(firestore, "users", attendeeId));
+    const fetchedAttendee = snapshot.data() as Attendee;
+    fetchedAttendee.id = attendeeId;
+    return fetchedAttendee;
+}
+
+export async function getAttendanceList(
+    groupId: GroupId,
+    eventId: EventId
+): Promise<Attendee[]> {
+    const fetchedEvent = await getEventById(groupId, eventId);
+    const attendanceList: Attendee[] = [];
+    fetchedEvent.attendees.forEach(async (attendeeId) => {
+        attendanceList.push(await getAttendeeById(attendeeId));
+    });
+    return attendanceList;
 }
 
 export async function setAttendeeGoing(
-  groupId: GroupId,
-  eventId: EventId,
-  attendeeId: AttendeeId
+    groupId: GroupId,
+    eventId: EventId,
+    attendeeId: AttendeeId
 ) {
-  updateDoc(doc(firestore, "Events", eventId), {
-    attendeesList: { attendeeId: true },
-  });
-  updateDoc(doc(firestore, "Attendee", attendeeId, groupId), {
-    eventId: true,
-  });
+    await updateDoc(doc(firestore, "groups", groupId, "events", eventId), {
+        attendees: arrayUnion(attendeeId),
+    });
+    // updateDoc(doc(firestore, "users", attendeeId, groupId), {
+    //     eventId: true,
+    // });
 }
 
 export async function addAttendeeGoing(
-  groupId: GroupId,
-  eventId: EventId,
-  firstName: string,
-  lastName: string
+    groupId: GroupId,
+    eventId: EventId,
+    firstName: string,
+    lastName: string
 ) {
-  addDoc(collection(firestore, "Groups", groupId), {
-    firstName: firstName,
-    lastName: lastName,
-  } as Attendee).then((doc) => {
-    setAttendeeGoing(groupId, eventId, doc.id);
-  });
+    addDoc(collection(firestore, "users"), {
+        firstName: firstName,
+        lastName: lastName,
+    } as Attendee).then((attendee) => {
+        setAttendeeGoing(groupId, eventId, attendee.id);
+    });
 }
 
 export async function removeAttendeeGoing(
-  groupId: GroupId,
-  eventId: EventId,
-  attendeeId: AttendeeId
+    groupId: GroupId,
+    eventId: EventId,
+    attendeeId: AttendeeId
 ) {
-  updateDoc(doc(firestore, "Events", eventId), {
-    attendeesList: { attendeeId: deleteField() },
-  });
-  updateDoc(doc(firestore, "Attendee", attendeeId, groupId), {
-    eventId: deleteField(),
-  });
+    await updateDoc(doc(firestore, "groups", groupId, "events", eventId), {
+        attendees: arrayRemove(attendeeId),
+    });
+    // updateDoc(doc(firestore, "Attendee", attendeeId, groupId), {
+    //     eventId: deleteField(),
+    // });
 }
