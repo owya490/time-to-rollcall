a2fc974d762b7cc8476bccf86523455594931382
Ian Chung <ian.chung@sowaustralia.com>
2024-02-20 01:08:09 +1100

    SOW-429: added create group function

--- frontend/src/lib/groups.ts
+++ frontend/src/lib/groups.ts
@@ -1,7 +1,9 @@
-import { doc, getDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc } from "firebase/firestore";
 import { convertToJavascript, firestore } from "@/lib/firebase";
 import { GroupModel } from "@/models/Group";
 import { getTags } from "./tags";
+import { addGroupToUserGroups } from "./users";
+import { UserId } from "@/models/User";
 
 export async function getGroups(groupIds?: string[]) {
   const groups: GroupModel[] = [];
@@ -18,4 +20,29 @@ export async function getGroup(groupId: string) {
   let groupDoc = await getDoc(doc(firestore, "groups", groupId));
   let tags = await getTags(groupId);
   return { ...(await convertToJavascript(groupDoc)), tags } as GroupModel;
+}
+
+// create group function
+// it should return created group
+// make a button that triggers this function
+// with hardcoded values
+// addDoc groups collection, object, { name }
+// loop through tagNames, call addDoc on the group -> tags collection {name: tagName}
+export async function createGroup(
+  name: string,
+  tagNames: string[],
+  id: UserId
+) {
+  const addedDoc = await addDoc(collection(firestore, "groups"), {
+    name,
+  });
+  await addGroupToUserGroups(id, addedDoc.id);
+  tagNames.map(
+    async (t) =>
+      await addDoc(collection(firestore, "groups", addedDoc.id, "tags"), {
+        name: t,
+      })
+  );
+
+  return getGroup(addedDoc.id);
 }
