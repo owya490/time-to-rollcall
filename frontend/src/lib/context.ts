21397a5ec71893eec8cfa3c40f6b60939d4d0142
= <kimchankwon@gmail.com>
2024-03-27 17:06:25 +1100

    fix null error

--- frontend/src/lib/context.ts
+++ frontend/src/lib/context.ts
@@ -4,5 +4,6 @@ import { Dispatch, SetStateAction, createContext } from "react";
 
 export const UserContext = createContext<User>(null);
 
-export const GroupContext =
-  createContext<[GroupModel, Dispatch<SetStateAction<GroupModel>>]>(null);
+export const GroupContext = createContext<
+  [GroupModel, Dispatch<SetStateAction<GroupModel>>]
+>([null, null]);
