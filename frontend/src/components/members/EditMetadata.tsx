ce1f6b6ed63dc25f5934845bf3018d7ae30b6716
Daniel Kim <kimchankwon@gmail.com>
2024-10-31 14:18:37 +1100

    order

--- frontend/src/components/members/EditMetadata.tsx
+++ frontend/src/components/members/EditMetadata.tsx
@@ -381,7 +381,10 @@ export default function EditMetadata({
                         <button
                           type="button"
                           className="bottom-2 fixed inline-flex mt-4 z-50 justify-center rounded-3xl border border-transparent bg-black px-4 py-2 text-sm font-medium text-white focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
-                          onClick={submit}
+                          onClick={() => {
+                            setOrder(false);
+                            submit();
+                          }}
                         >
                           Update
                         </button>
